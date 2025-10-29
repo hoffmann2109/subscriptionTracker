93458eaf7f6b6cf88bb8bdb5bc92da9175800d1d
Thomas Hoffmann <t.hoffmann2109@gmx.at>
2025-10-29 16:30:59 +0100

    Added some unit Tests

---
+++ AboTracker.Tests/CalculatorTests.cs
@@ -0,0 +1,73 @@
+using AboTracker.Logic;
+using AboTracker.Model;
+using Xunit;
+
+namespace AboTracker.Tests
+{
+    public class CalculatorTests
+    {
+        private readonly List<Subscription> _aboList = [];
+
+        public CalculatorTests()
+        {
+            var sub1 = new Subscription{
+                Name = "Test 1", 
+                Amount = 1, 
+                PaymentPeriod = "Daily", 
+                PurchaseDate = "28.10.2025", 
+                NextPaymentDate = "29.10.2025", 
+                Category = "Entertainment"
+            };
+            
+            var sub2 = new Subscription{
+                Name = "Test 2", 
+                Amount = 3, 
+                PaymentPeriod = "Weekly", 
+                PurchaseDate = "10.10.2025", 
+                NextPaymentDate = "17.10.2025", 
+                Category = "Entertainment"
+            };
+            
+            var sub3 = new Subscription{
+                Name = "Test 3", 
+                Amount = 10, 
+                PaymentPeriod = "Monthly", 
+                PurchaseDate = "28.10.2025", 
+                NextPaymentDate = "28.11.2025", 
+                Category = "Entertainment"
+            };
+            
+            var sub4 = new Subscription{
+                Name = "Test 4", 
+                Amount = 40, 
+                PaymentPeriod = "Quarterly", 
+                PurchaseDate = "28.01.2025", 
+                NextPaymentDate = "28.04.2025", 
+                Category = "Entertainment"
+            };
+            
+            var sub5 = new Subscription{
+                Name = "Test 5", 
+                Amount = 120, 
+                PaymentPeriod = "Yearly", 
+                PurchaseDate = "28.01.2025", 
+                NextPaymentDate = "28.04.2025", 
+                Category = "Entertainment"
+            };
+            
+            _aboList.Add(sub1);
+            _aboList.Add(sub2);
+            _aboList.Add(sub3);
+            _aboList.Add(sub4);
+            _aboList.Add(sub5);
+            
+        }
+
+        [Fact]
+        public void CalculateMonthlySum_ComputesCorrectSum()
+        {
+            Assert.Equal(76.75,Calculator.CalculateMonthlySum(_aboList));
+        }
+        
+    }
+}
--- AboTracker/Logic/Utility.cs
+++ AboTracker/Logic/Utility.cs
@@ -2,8 +2,13 @@ using System.Text;
 
 namespace AboTracker.Logic;
 
-public class Utility
+public abstract class Utility
 {
+    public static void ExportToCsv()
+    {
+        
+    }
+    
     public static string ToUpperFirst(string str)
     {
         return char.ToUpper(str[0]) + str[1..];
